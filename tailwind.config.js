a930858745920662a5282d8ca10eef67effa4a93
Rachel Dooley <rae1821@gmail.com>
2022-06-07 09:47:55 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Raleway: ["Raleway", "sans-serif"],
+        Sacramento: ["Sacramento", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+}
