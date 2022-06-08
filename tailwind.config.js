fd38d0f84c56e83a1b7cc4b87086a95e7f16db47
Rachel Dooley <rae1821@gmail.com>
2022-06-08 16:06:54 -0400

    Changed some colors, added sloth pic, built footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
       fontFamily: {
         Raleway: ["Raleway", "sans-serif"],
         Sacramento: ["Sacramento", "cursive"],
+        Licorice: ["Licorice", "cursive"],
+        Pacifico: ["Pacifico", "cursive"],
+        RobotoCondensed: ["Roboto Condensed", "sans-serif"],
+        BadScript: ["Bad Script", "cursive"],
       },
     },
   },
