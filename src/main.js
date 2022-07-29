00fe28a24d58636e0d14c94aa677794139257642
rachel_dooley <rae1821@gmail.com>
2022-07-29 16:06:30 -0400

    Added individual pages for each project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 
+//Toggle mobile nave menu on click
 const button = document.querySelector('svg.w-6');
 
 button.addEventListener('click', function() {
@@ -7,3 +8,32 @@ button.addEventListener('click', function() {
     mobileMenu.classList.toggle('block');
    
 });
+
+
+//Scroll To Top Button 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+
+//When user scrolls down 20px from top of page show button 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(
+        document.body.scrollTop > 20 || 
+        document.documentElement.scrollTop > 20
+        ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+//When user clicks button, scroll to top of page 
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0; //For safari
+    document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE, and Opera
+}
